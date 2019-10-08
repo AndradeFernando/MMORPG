bb0b2b4eb2e887e57f85b5d125eab56043d26089
FTA1984NOW <fernando_andrade00@hotmail.com>
2019-10-08 12:02:08 -0300

    initial project version

--- MMORPG/app/controllers/index.js
+++ MMORPG/app/controllers/index.js
@@ -5,8 +5,8 @@ module.exports.index = function(application, req, res) {
 module.exports.autenticar = function(application, req, res) {
 	
 	var dadosForm = req.body;
-	req.assert('usuario','Usuário não pode ser vazio').notEmpty();
-	req.assert('senha','Senha não pode ser vazio').notEmpty();
+	req.assert('usuario','Usuário não pode ser vazio po!!!').notEmpty();
+	req.assert('senha','Senha não pode ser vazio po!!!!!').notEmpty();
 	
 	var erros = req.validationErrors();
 	if(erros){
